fc2b829bd1aa82cf6d4d23f0f1c6872f58e141f8
Nihaal Manesia <nihaal419@gmail.com>
2016-02-24 19:30:00 -0600

    Initial Commit

---
+++ KnightTimeTests/KnightTimeTests.swift
@@ -0,0 +1,36 @@
+//
+//  KnightTimeTests.swift
+//  KnightTimeTests
+//
+//  Created by Nihaal Manesia on 2/24/16.
+//  Copyright © 2016 N Squared Productions. All rights reserved.
+//
+
+import XCTest
+@testable import KnightTime
+
+class KnightTimeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
