c73c7a20736e464bc41e6a667249cf75a9e70201
Nihaal Manesia <nihaal@manesia.org>
2026-06-26 22:17:33 -0500

    Initial Commit

---
+++ KnightTime/KnightTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnightTimeApp.swift
+//  KnightTime
+//
+//  Created by Nihaal Manesia on 6/26/26.
+//
+
+import SwiftUI
+
+@main
+struct KnightTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KnightTimeTests/KnightTimeTests.swift
@@ -0,0 +1,18 @@
+//
+//  KnightTimeTests.swift
+//  KnightTimeTests
+//
+//  Created by Nihaal Manesia on 6/26/26.
+//
+
+import Testing
+
+struct KnightTimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
